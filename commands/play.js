4eadf7e8ee6e3a476e4b14e00c560847007571bb
AugustoV2 <augustovarghese@gmail.com>
2024-06-02 02:35:36 +0530

    4

--- commands/play.js
+++ commands/play.js
@@ -1,59 +1,40 @@
-
-const { REST, Routes, SlashCommandAssertions, SlashCommandBuilder } = require('discord.js');
-
+const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
-//const { QueryType } = require("discord-player")
-
-
-const rest = new REST({ version: '10' }).setToken("MTI0NjM3MjQ5MDI4MDk2NDExNg.G05B65.COSK-Bjhm9AcxeOr8XuOGlxAsYyZzZxOM9xyM8");
+const { QueryType } = require("discord-player")
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("play")
-        .setDescription("Play a song from url")
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("song")
-                .setDescription("name of the song")
-                .addStringOption((option) =>
-                    option
+	data: new SlashCommandBuilder()
+		.setName("play")
+		.setDescription("loads songs from youtube")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("song")
+				.setDescription("Loads a single song from a url")
+				.addStringOption((option) => option.setName("url").setDescription("the song's url").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("playlist")
+				.setDescription("Loads a playlist of songs from a url")
+				.addStringOption((option) => option.setName("url").setDescription("the playlist's url").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("search")
+				.setDescription("Searches for sogn based on provided keywords")
+				.addStringOption((option) =>
+					option.setName("searchterms").setDescription("the search keywords").setRequired(true)
+				)
+		),
+	run: async ({ client, interaction }) => {
+		if (!interaction.member.voice.channel) return interaction.editReply("You need to be in a VC to use this command")
 
-                        .setName("url")
-                        .setDescription("the songs url")
-                        .setRequired(true))
-        )
-      
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("song")
-                .setDescription("name of the song")
-                .addStringOption((option) =>
-                    option
+		const queue = await client.player.createQueue(interaction.guild)
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-                        .setName("url")
-                        .setDescription("the songs url")
-                        .setRequired(true))
-        )
-        .addSubcommand((subcommand) =>
-            subcommand
-                .setName("search")
-                .setDescription("Search name of the song")
-                .addStringOption((option) =>
-                    option
+		let embed = new MessageEmbed()
 
-                        .setName("searchterms")
-                        .setDescription("search the keywords")
-                        .setRequired(true)  )
-        ),
-    run: async ({ client, interaction }) => {
-        if (!interaction.member.voice.channel) return interaction.editReply("You need to be in a VC to use this command")
-
-        const queue = await client.player.createQueue(interaction.guild)
-        if (!queue.connection) await queue.connect(interaction.member.voice.channel)
-
-        let embed = new MessageEmbed()
-
-        if (interaction.options.getSubcommand() === "song") {
+		if (interaction.options.getSubcommand() === "song") {
             let url = interaction.options.getString("url")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
@@ -61,14 +42,49 @@ module.exports = {
             })
             if (result.tracks.length === 0)
                 return interaction.editReply("No results")
-
+            
             const song = result.tracks[0]
             await queue.addTrack(song)
             embed
                 .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
                 .setThumbnail(song.thumbnail)
-                .setFooter({ text: `Duration: ${song.duration}` })
+                .setFooter({ text: `Duration: ${song.duration}`})
 
-        }
-    }
+		} else if (interaction.options.getSubcommand() === "playlist") {
+            let url = interaction.options.getString("url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_PLAYLIST
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
+                .setThumbnail(playlist.thumbnail)
+		} else if (interaction.options.getSubcommand() === "search") {
+            let url = interaction.options.getString("searchterms")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.AUTO
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+		}
+        if (!queue.playing) await queue.play()
+        await interaction.editReply({
+            embeds: [embed]
+        })
+	},
 }
--- index.js
+++ index.js
@@ -1,39 +1,86 @@
 
-const{ Client , GatewayIntentBits } = require("discord.js");
-//const { Player } = require("discord-player")
-const client = new Client({ 
+const { Client, GatewayIntentBits } = require("discord.js");
+const fs = require("fs")
+const dotenv = require("dotenv")
+dotenv.config()
+const TOKEN = process.env.TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+const GUILD_ID = process.env.GUILD_ID
+const { Player } = require("discord-player")
+const client = new Client({
     intents:
-     [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildVoiceStates
-    ] 
+        [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildVoiceStates
+        ]
 });
 
-client.on('messageCreate',message =>{
-    if(message.author.bot) return;
-    //console.log(message.content);
-    
-   
-    message.reply({
-        content: "hello mone"
-    });
 
-});
 
-client.on('interactionCreate',interaction =>{
-    if(interaction.commandName === 'ping'){
-    interaction.reply("blaaa");
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
     }
+})
+let commands = []
 
-    if(interaction.commandName === 'nokkuman'){
-        interaction.reply("blaaa");
+const slashFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))
+for (const file of slashFiles) {
+    const slashcmd = require(`./commands/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJson())
+}
+
+
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: '10' }).setToken(TOKEN);
+    console.log("Deploying commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+        .then(() => {
+            console.log("Successfully loaded")
+            process.exit(0)
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(err)
+                process.exit(1)
+            }
+        })
+}
+else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+    })
+    client.on('messageCreate', message => {
+        if (message.author.bot) return;
+        //console.log(message.content);
+
+
+        message.reply({
+            content: "hello mone"
+        });
+
+    })
+
+    client.on('interactionCreate', interaction => {
+        async function handleCommand() {
+
+            if (!interaction.commandName) return interaction.reply("invalid command");
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+
+            if (!slashcmd) interaction.reply("Not a valid slash command");
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
         }
-        if (interaction.commandName === 'play') return
-
-        const slashcmd = client.slashcommands.get(interaction.commandName)
-});
+        handleCommand()
+    })
 
 
-client.login("MTI0NjM3MjQ5MDI4MDk2NDExNg.G05B65.COSK-Bjhm9AcxeOr8XuOGlxAsYyZzZxOM9xyM8");
+    client.login("MTI0NjM3MjQ5MDI4MDk2NDExNg.G05B65.COSK-Bjhm9AcxeOr8XuOGlxAsYyZzZxOM9xyM8");
+}
